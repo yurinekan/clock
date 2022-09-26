2578707b5ace9a1370098c0fe9a5ca6b95e625f2
Yuri Nekan <yurinekanfts@hotmail.com>
2022-09-26 00:52:31 -0300

    background and date added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,41 @@ import './App.scss';
 function App() {
 
   function loop() {
-    setInterval(displayTime,1000)
+    setInterval(displayTime, 1000)
   }
   loop()
-  function displayTime(){
+  
+  function displayTime() {
     const timeNow = new Date();
+
+    let day = timeNow.toLocaleString('default', { weekday: 'short' });
+    let month = timeNow.toLocaleString('default', { month: 'long' });
+    let dayInNumber = timeNow.toLocaleString('default', { day: 'numeric' });
     
+    let dateStr = (day + ', ' + dayInNumber + ' de ' + month);
+    document.getElementById('date').innerText = dateStr;
+
     let hours = timeNow.getHours();
     let minutes = timeNow.getMinutes();
     let seconds = timeNow.getSeconds();
-    
+
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
-    
+
     let timeStr = hours + ":" + minutes + ":" + seconds;
-    
+
     document.getElementById('clock').innerText = timeStr;
   }
-  
+
   return (
-    <div id="clock" class="glow">00:00:00</div>
+    <div className='container'>
+      <p id="date" className="glow">
+      </p>
+      <h2 id="clock" className="glow">
+      </h2>
+      <img src="https://picsum.photos/1920/1080" alt="" />
+    </div>
   );
 }
 
